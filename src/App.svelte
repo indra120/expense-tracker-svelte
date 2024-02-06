096dc24d6cd75a1f5245331b3431f9f29922179b
indra120 <indrapranata120@gmail.com>
2024-02-06 21:25:16 +0800

    Compute income & expenses

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import TransactionList from "./lib/TransactionList.svelte"
   import Balance from "./lib/Balance.svelte"
+  import IncomeExpenses from "./lib/IncomeExpenses.svelte"
 </script>
 
 <h2>Expense Tracker</h2>
 
 <div class="container">
   <Balance />
+  <IncomeExpenses />
   <TransactionList />
 </div>
---
+++ src/lib/IncomeExpenses.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { transactions, type Transaction } from "../store"
+
+  const calculate = (arr: Transaction[]) =>
+    arr.reduce((acc, data) => acc + data.amount, 0).toFixed(2)
+
+  $: income = calculate($transactions.filter((data) => data.amount > 0))
+  $: expenses = calculate($transactions.filter((data) => data.amount < 0))
+</script>
+
+<div class="inc-exp-container">
+  <div>
+    <h4>Income</h4>
+    <p class="money plus">+${income}</p>
+  </div>
+
+  <div>
+    <h4>Expense</h4>
+    <p class="money minus">-${+expenses * -1}</p>
+  </div>
+</div>
