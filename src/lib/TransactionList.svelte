2720ea351f6d2574430ba9360289ca3639588665
indra120 <indrapranata120@gmail.com>
2024-02-06 22:10:31 +0800

    Add feature to delete each transaction

--- src/lib/TransactionList.svelte
+++ src/lib/TransactionList.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+  import { toast } from "svoast"
   import { transactions } from "../store"
+
+  const deleteTransactionById = (id: number) => () => {
+    transactions.update((lists) => lists.filter((item) => item.id !== id))
+    toast.success("Transaction has been deleted")
+  }
 </script>
 
 <h3>History</h3>
@@ -9,6 +15,7 @@
     <li class="list" class:minus={amount < 0} class:plus={amount > 0}>
       {text}
       <span>{amount < 0 ? "-" : ""}${Math.abs(amount)}</span>
+      <button class="delete-btn" on:click={deleteTransactionById(id)}>x</button>
     </li>
   {/each}
 </ul>
