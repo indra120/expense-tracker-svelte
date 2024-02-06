d7cee4246d4bacae88adbc39d6797f58295b260f
indra120 <indrapranata120@gmail.com>
2024-02-06 21:13:23 +0800

    Compute total balance

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import TransactionList from "./lib/TransactionList.svelte"
+  import Balance from "./lib/Balance.svelte"
 </script>
 
 <h2>Expense Tracker</h2>
 
 <div class="container">
+  <Balance />
   <TransactionList />
 </div>
---
+++ src/lib/Balance.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { transactions } from "../store"
+
+  $: total = $transactions.reduce((acc, data) => acc + data.amount, 0)
+</script>
+
+<h4>Your Balance</h4>
+<h1>${total}</h1>
